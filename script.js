82f31434c04fba3249486451bcf487fcee878ab0
Денис Кирилюк <denkiril@gmail.com>
2018-07-12 16:01:16 +0300

    last lesson

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ $(function() {
         $('.post').show(500);
     });
 });
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 8
+    });
+  });
+
+if($(window).width() < 420){
+    $(".owl-carousel").owlCarousel({
+        items: 3
+    });
+  }
